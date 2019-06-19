78d11057744d6053c2ac950dc665a51ab7b00b70
Anthony Mace <anthony.mace@anthonymaceml1.infusionsoft.phx>
2019-06-19 14:54:30 -0700

    feat(project): initial commit

---
+++ src/test/java/com/anthonymace/contacttasks/ContactTasksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anthonymace.contacttasks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactTasksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
